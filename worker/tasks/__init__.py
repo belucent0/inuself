1e6876600086c8e55fd59e2408ddc5825e7c50b3
belucent0 <belucent0@gmail.com>
2026-02-15 01:18:16 +0900

    feat(worker): run WPI report generation with LangGraph pipeline

--- worker/tasks/__init__.py
+++ worker/tasks/__init__.py
@@ -1,10 +1,13 @@
 """Celery 태스크 모듈."""
+
 from .asr_task import process_asr_task
 from .llm_task import process_llm_task
 from .ocr_task import process_ocr_task
+from .wpi_report_task import process_wpi_report_task
 
 __all__ = [
     "process_asr_task",
     "process_llm_task",
     "process_ocr_task",
+    "process_wpi_report_task",
 ]
